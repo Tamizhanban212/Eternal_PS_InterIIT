4970e85923af6d6de9492a0316ac1b91fcecac38
Tamizhanban212 <tamizhanbanag@gmail.com>
2025-12-09 00:31:54 +0530

    change z_test.py

--- z_test.py
+++ z_test.py
@@ -6,31 +6,12 @@ init_z_axis()
 
 try:
     # Move 50 cm UP
-    z_axis(50, 1)
+    z_axis(10, 1)
     time.sleep(2)
     
     # Move 30 cm DOWN
-    z_axis(30, 0)
-    time.sleep(2)
-    
-    # Move 15 cm UP
-    z_axis(15, 1)
-    time.sleep(2)
-    
-    # Move 10 cm DOWN
     z_axis(10, 0)
     time.sleep(2)
-    
-    # Move 25 cm UP
-    z_axis(25, 1)
-    time.sleep(2)
-    
-    # Move 20 cm DOWN
-    z_axis(20, 0)
-    time.sleep(2)
-    
-    # Move 5 cm UP
-    z_axis(5, 1)
     
 finally:
     cleanup_z_axis()
